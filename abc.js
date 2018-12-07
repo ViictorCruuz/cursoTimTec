811ebe4b9f5f6125325d7b68757ac4e1f6fe0b7d
Victor Cruz - Local <victor.cruz@geru.com.br>
2018-12-07 14:59:17 -0200

    course of Javascript

---
+++ abc.js
@@ -0,0 +1,17 @@
+// function somar(x,y) {
+//     var resultado = x + y;
+//     console.log(resultado);
+// }
+
+function javascript() {
+    var retorno = 'Welcome in my new site'
+    console.log(retorno);
+}
+
+
+var atalho = document.getElementById('clique')
+atalho.addEventListener('click', back, false);
+
+function back() {
+    console.log('Button test clicked')
+}
