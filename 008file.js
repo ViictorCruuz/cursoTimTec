9f70aad5893076995ee9e1e62e18cfca58146560
Victor Cruz - Local <victor.cruz@geru.com.br>
2018-12-10 18:32:51 -0200

    10/12

--- 008file.js
+++ 008file.js
@@ -8,7 +8,7 @@ function clique(){
 function troca(cor){
     if(cor == "vermelho"){
         document.getElementById("fundo").style.backgroundColor="red";
-        // document.getElementById("quadrado").style.backgroundColor="#f0f";
+        document.getElementById("quadrado").style.backgroundColor="#f0f";
     }else if(cor == "verde"){
         document.getElementById("fundo").style.backgroundColor="green";
     }else{
---
+++ 010file.js
@@ -0,0 +1,6 @@
+//  Javascript Document
+
+function contaCaracteres(){
+
+    document.forms[0].elements[1].value = document.forms[0].elements[0].value.length+1;
+}
